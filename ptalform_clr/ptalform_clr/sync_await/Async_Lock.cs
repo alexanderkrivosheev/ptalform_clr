cfe09abe18b0232a3ee016fce8a2bf3983e527cd
alexzoa <124262010+alexzoa@users.noreply.github.com>
2024-05-28 10:45:50 +0200

    Added Asyn_lock

---
+++ ptalform_clr/ptalform_clr/sync_await/Async_Lock.cs
@@ -0,0 +1,41 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace ptalform_clr.statements
+{
+    [MemoryDiagnoser]
+    public class Async_Lock
+    {
+        private static readonly object _lock = new object();
+
+        public async Task ExampleMethodAsync()
+        {
+            lock (_lock)
+            {
+                // Your asynchronous code here
+                //await SomeAsyncOperation();
+            }
+        }
+
+        public async Task SomeAsyncOperation()
+        {
+            await Task.Delay(5000);
+        }
+
+        private static readonly SemaphoreSlim semaphore = new SemaphoreSlim(1, 1);
+
+        public async Task ExampleMethodAsync2()
+        {
+            await semaphore.WaitAsync();
+            try
+            {
+                // Your asynchronous code here
+                await SomeAsyncOperation();
+            }
+            finally
+            {
+                semaphore.Release();
+            }
+        }
+
+    }
+}
