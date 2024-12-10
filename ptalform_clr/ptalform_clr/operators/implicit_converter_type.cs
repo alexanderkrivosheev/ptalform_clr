0ec82ae4761cd9c2975b37ccf71dabe0ee211cd3
Alexander Krivosheev <alexander.krivosheev@hotmail.com>
2024-12-10 15:41:00 +0100

    implicit_converter_type

---
+++ ptalform_clr/ptalform_clr/operators/implicit_converter_type.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ptalform_clr.operators
+{
+    internal class implicit_converter_type
+    {
+        public static IEnumerable<Result<int>> GetResults()
+        {
+            yield return new Result<int>(0);
+
+            yield return 2;
+        }
+    }
+
+    public class Result<TValue>
+    {
+        private readonly TValue result;
+
+        public Result(TValue result)
+        {
+            this.result = result;
+        }
+
+        public static implicit operator Result<TValue>(TValue result) 
+        {
+            return new Result<TValue>(result);
+        }
+    }       
+}
